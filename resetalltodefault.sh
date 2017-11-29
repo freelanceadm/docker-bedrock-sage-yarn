f2fc5004bb9d82b74a3870ba49c468542ac8e7c4
root <root@wsr-rbw-uch-01.rainbow.g310>
2017-11-29 19:11:26 +0300

    Sage theme is added to website.

---
+++ resetalltodefault.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Stopping containers."
+docker-compose down
+
+echo "Clean mysql dir"
+rm -rf mysql/*
+
+echo "Clean themes"
+rm -rf themes/*
+
+echo "Clean www directory"
+rm -rf www/*
